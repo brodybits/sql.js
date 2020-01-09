a47504379fb7bf0d722f88f37fe80e439e368c10
Christopher J. Brody <chris@brody.consulting>
2020-01-09 12:44:52 -0500

    quick updates for EU character support
    feature from https://github.com/brodybits/sqlite3-eu

--- src/custom_extensions/custom_extensions.c
+++ src/custom_extensions/custom_extensions.c
@@ -2,7 +2,12 @@
 
 /** FUTURE TBD Build this module with any custom extension module(s) included */
 
+#include "sqlite3_eu.c"
+
 int custom_extensions(sqlite3 * db)
 {
+  // ref: https://github.com/brodybits/sqlite3-eu
+  sqlite3_eu_init(db, "UPPER", "LOWER");
+
   return SQLITE_OK;
 }
