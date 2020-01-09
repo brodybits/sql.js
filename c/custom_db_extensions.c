19ec96278bc41eafe9f9e835d9c04e14004676f9
Christopher J. Brody <chris@brody.consulting>
2020-01-08 21:35:35 -0500

    Quick updates for EU character support
    feature from https://github.com/brodybits/sqlite3-eu
    
    FUTURE TODO: VERIFY that the following succeeds on the command line:
    
        make clean-all && make && npm test

--- c/custom_db_extensions.c
+++ c/custom_db_extensions.c
@@ -2,7 +2,11 @@
 
 /** FUTURE TBD Build this module with any custom extension module(s) included */
 
+#include "sqlite3_eu.c"
+
 int custom_db_extensions(sqlite3 * db)
 {
+  sqlite3_eu_init(db, "UPPER", "LOWER");
+
   return SQLITE_OK;
 }
